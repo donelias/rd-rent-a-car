b28550aa869bb0290a6031276f563533d6f789ed
Hector Galindez <hectorgalindez02@gmail.com>
2019-06-19 08:56:40 -0400

    Configuration of env file codeship

--- config/database.php
+++ config/database.php
@@ -66,6 +66,20 @@ return [
             'engine' => null,
         ],
 
+        'mysql_tests' => [
+            'driver' => 'mysql',
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('MYSQL_USER','DB_USERNAME', 'forge'),
+            'password' => env('MYSQL_PASSWORD','DB_PASSWORD', ''),
+            'charset' => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix' => '',
+            'strict' => true,
+            'engine' => null,
+        ],
+
         'pgsql' => [
             'driver' => 'pgsql',
             'host' => env('DB_HOST', '127.0.0.1'),
